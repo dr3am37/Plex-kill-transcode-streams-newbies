0793c35bc75d53a5f125d42a3ca8be108b620ccf
martin <martin.labat92@gmail.com>
2022-02-23 13:47:41 +0100

    add script arguments

--- README.md
+++ README.md
@@ -4,16 +4,18 @@ This script allows you to stop streams that use trancoding.
 Prohibits the use of plex web and Plex Media Player.
 Allow all users to transcode for SD streams
 
-## Requirements
+### Requirements
 
 Python3 / requests (usually included with python3)<br>
 Tautulli (fantastic app that you should be using and must for this script anyway)<br>
 PlexPass
 
 
+## Method 1 : run a shell script in background
 
+The script checks every 20 seconds streams activities.
 
-## Install requirements :
+### Install requirements :
 
 run
 
@@ -21,28 +23,35 @@ run
    user:~/plex-kill-transcode-streams$ sudo pip install -r requirements.txt
    ```
 
-## Edit the bash script (KillstreamLoop.sh):
+### Edit the bash script (KillstreamLoop.sh):
 
 In this file you need to edit this line: "cd /YOURPATH/killstreamloop.sh && python3 killstream.py" <br>
 Specifically you need to edit this part: "/YOURPATH/Tautulli_IP_Enforcer" <br>
 
-## To allow accounts to transcode:
+### To allow accounts to transcode:
 
 Edit the file "authorized.txt" and add the username id followed by a semicolon and the limit of unique IP addresses that are allowed. <br>For example:<br><br>52252389<br>56428612<br><br>will allow 2 precise accounts to transcode streams for this Plex server. If a user is not in the text file then they can't transcode streams. They have to use direct stream or direct play.
 
 
-# If you don't want to run the shell script in background :
+## Method 2 : tautulli run itself the script  :
 
+Tautulli run the script when a stream starts, resumes or when a stream changes its transcode decision.
 
-## Install python requirements
+### Install requirements
 
    ```shell
    user:~/plex-kill-transcode-streams$ sudo pip install -r requirements.txt
    ```
 
-## Edit your tautulli settings
+### Edit your tautulli settings
 
 Add a new notifications agent called killstream and edit the configuration and triggers like the following images:
+You can add the following script arguments :
+
+-w : Kill all unauthorized streams which use Plex Web as client
+-m : Kills all unauthorized streams which use Plex Media Player as client
+-s : Allow streams to transcode to sd resolution
+
 
 <p align="center">
   <img src="./img/settings.png" width="350" title="settings">
@@ -51,7 +60,9 @@ Add a new notifications agent called killstream and edit the configuration and t
 <p align="center">
   <img src="./img/triggers.png" width="350" title="triggers">
 </p>
-
+<p align="center">
+  <img src="./img/arguments.png" width="350" title="arguments">
+</p>
 
 <br><br><br>
 Script based on https://github.com/Dosk3n/Tautulli_IP_Enforcer
--- killstream.py
+++ killstream.py
@@ -26,10 +26,10 @@ def GetConcSettings():
     except:
         pass
 
-def EnforceConcurrent(tActivitySessions, tapi, concurrentMsg):
+def EnforceConcurrent(tActivitySessions, tapi, concurrentMsg,web,mediaplayer,sd):
     try:
         concSettings = GetConcSettings()
-        
+
         dataByUser = {}
         # Loop through each session and add to a dictionary where key is the username
         for session in tActivitySessions:
@@ -40,11 +40,13 @@ def EnforceConcurrent(tActivitySessions, tapi, concurrentMsg):
                 # User isnt in dictionary so add key data pair with username as key and an ampty array as data
                 dataByUser[session["user"]] = []
                 dataByUser[session["user"]].append(session) # Append that session to the array of that user
-        
+
         # By this point we have a dictionary of current users with their apropriate sessions
-        
         for cle,valeur in dataByUser.items():
-            
+
+
+
+            print(valeur)
             for j in range(len(valeur)) :
 
                 userid = valeur[j].get('user_id')
@@ -56,16 +58,16 @@ def EnforceConcurrent(tActivitySessions, tapi, concurrentMsg):
 
                     print(str(userid) + " : User unauthorized")
 
-                    if(valeur[j].get('product') == 'Plex Web' or valeur[j].get('product') == 'Plex Media Player'):
-                        if(valeur[j].get('product') == 'Plex Media Player') :
+                    if(mediaplayer == True or web == True):
+                        if(valeur[j].get('product') == 'Plex Media Player' and mediaplayer == True) :
                             concurrentMsg = "PLEX ENFORCER - YOUR MEDIA PLAYER IS DEPRECATED"
-                        if(valeur[j].get('product') == 'Plex Web') :
+                        if(valeur[j].get('product') == 'Plex Web' and web == True) :
                             concurrentMsg = "PLEX ENFORCER - YOUR ARE NOT AUTHORIZED TO USE PLEX WEB"
                         tapi.TerminateSession(valeur[j].get('session_key'), concurrentMsg)
                         print(str(userid) + " : Terminating sessions")
                         EnforcementLogger(valeur[j].get('friendly_name'), "transcode")
 
-                    elif(valeur[j].get('video_decision') == 'transcode' and valeur[j].get('state') != 'paused' and  (valeur[j].get(stream_video_resolution) != 'sd' ) ):
+                    elif(valeur[j].get('video_decision') == 'transcode' and valeur[j].get('state') != 'paused' and (( sd == True) and (valeur[j].get(stream_video_resolution) != 'sd' ) ) ):
 
                         tapi.TerminateSession(valeur[j].get('session_key'), concurrentMsg)
                         print(str(userid) + " : Terminating sessions")
@@ -73,40 +75,63 @@ def EnforceConcurrent(tActivitySessions, tapi, concurrentMsg):
 
                 else :
                     print(str(userid) + " : User authorized")
-                        
+
+
+
+
+
     except:
         pass
-    
 
 
-def EnforcementLogger(message):
+
+def EnforcementLogger(message, reason):
     try:
         now = datetime.datetime.now()
         preStringDate = now.strftime("%Y-%m-%d %H:%M:%S")
-        f = open("enforce_log.txt", "a")
-        f.write(preStringDate + ": " + message + "\n")
-        f.close()
+
+        if(reason == "transcode") :
+            f = open("enforce_log.txt", "a")
+            f.write(preStringDate  + " : " + message + "\n")
+            f.close()
+        else :
+            f = open("multiple_streams.txt", "a")
+            f.write(reason  + " : " + message + "\n")
+            f.close()
 
     except:
         print("There was a problem logging the error")
         pass
 
-def Main():
+if __name__ == "__main__":
     print("\nStarting Killstream")
+    mediaplayer = False
+    web = False
+    sd = False
+    parser = argparse.ArgumentParser(description='monitor plex activity')
+    parser.add_argument('-web', '-w', help='Kill streams to unauthorized web clients', action='store_true')
+    parser.add_argument('-sd', '-s', help='Allow streams to transcode to sd resolution', action='store_true')
+    parser.add_argument('-mediaplayer', '-m', help='Kill streams to unauthorized Plex Media Player', action='store_true')
+    args = parser.parse_args()
+    if args.web == True:
+        web = True
+    if args.mediaplayer == True:
+        mediaplayer = True
+    if args.sd == True:
+        sd = True
     tIP, tPORT, tAPIKEY, blackListMsg, concurrentMsg = GetTautInfo()
     tapi = TautulliApiHandler(tIP, tPORT, tAPIKEY)
     tActivityData = tapi.GetActivity()
     if(int(tActivityData["stream_count"]) > 0):
         print("There are currently " + str(tActivityData["stream_count"]) + " streams active")
 
+        EnforceIpBans(tActivityData["sessions"], tapi, blackListMsg) # Check and Enforce IP bans
 
 
-        EnforceConcurrent(tActivityData["sessions"], tapi, concurrentMsg) # Check and Enforce Concurrent Limits
+
+        EnforceConcurrent(tActivityData["sessions"], tapi, concurrentMsg,web,mediaplayer,sd) # Check and Enforce Concurrent Limits
 
     else:
         print("No streams found")
-    
 
         
-
-Main()
